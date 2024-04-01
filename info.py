95abd2d62a6b02061cb68a2eb888a1024dc9634d
MINHAJ OFFICIAL <164954898+MINHAJ-WORLD@users.noreply.github.com>
2024-04-02 01:23:05 +0600

    Create info.py

---
+++ info.py
@@ -0,0 +1,20 @@
+#--------এইটা হইলো tiktok user info code---------#
+#--------নাম এবং logo change করে নিয়েন--------#
+
+import requests
+
+def tiktok():
+    logo = (
+    print(logo)
+    user_input = input('Enter Tiktok Username: ')
+    url = f'http://localhost:8080/tiktok-info.php?username={user_input}'
+    
+    try:
+        req = requests.get(url).json()
+        
+        print(req)
+        
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+tiktok()
